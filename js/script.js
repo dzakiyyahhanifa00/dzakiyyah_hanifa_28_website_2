01d1e0390c2f7276329fb3f4cdf68df447b9a034
dzakiyyahhanifa00 <dzakiyyahhanifa081@gmail.com>
2025-08-08 05:43:51 +0700

    lagi nieh?

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-// // Toogle  class active 
+// Toogle  class active 
 
-// const navbarNav = document.querySelector('.navbar-nav');
+const navbarNav = document.querySelector('.navbar-nav');
 
-// // Ketika hamburger di klik
-// document.querySelector('#hamburger-menu').onclick = () => {
-//     navbarNav.classList.toggle('active');
-// };
+// Ketika hamburger di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
 
-// // Klik di luar sidebar untuk menghilangkan nav
-// const hamburger = document.querySelector('#hamburger-menu');
+// Klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector('#hamburger-menu');
 
-// document.addEventListener('click', function (e) {
-//     if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-//         navbarNav.classList.remove('active');
-//     }
-// });
+document.addEventListener('click', function (e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
